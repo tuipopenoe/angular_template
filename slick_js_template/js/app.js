62c867b841496531a284451677e266e37b205655
Tui Popenoe <tuipopenoe@gmail.com>
2015-01-10 23:59:29 -0600

    Bower installed components

--- slick_js_template/js/app.js
+++ slick_js_template/js/app.js
@@ -1,3 +1,14 @@
-use strict;
+// © Tui Popenoe, 2014
+var slick_js_template = angular.module('slick_js_template', ['slick']);
 
-angular.module('slick_js_template', ['slick', 'hljs']);
+angular.module('slick_js_template')
+  .controller('MainCtrl', function($scope, $timeout){
+      $scope.languages = [
+        'ECMAScript6',
+        'Java',
+        'Python',
+        'C++',
+        'Ruby',
+        'HTML5',
+      ];
+  });
