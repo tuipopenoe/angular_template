7f5cffdf8d2afd643433febafe7801980a0aec41
Tui Popenoe <tuipopenoe@gmail.com>
2015-02-19 23:43:33 -0600

    Gruntfile fixes

--- base_angular_template/Gruntfile.js
+++ base_angular_template/Gruntfile.js
@@ -1,21 +1,93 @@
 module.exports = function(grunt) {
-    // Project configuration
+    // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        watch: {
+            files: ['**/*.hmlt', '**/*.css'],
+            tasks: ['htmlmin', 'imagemin', 'compress']
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.html'],
+                    dest: 'dist/'
+                }]
             },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%=pkg.name %>.min.js'
+            dev: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.html'],
+                    dest: 'dev/'
+                }]
+            }
+        },
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    svgoPlugins: [
+                        { removeViewBox: true },
+                        { removeUselessStrokeAndFill: true },
+                        { removeEmptyAttrs: true }
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.{png,jpg,gif,svg}'],
+                    dest: 'dev/'
+                }]
+            },
+            dev: {
+                options: {
+                    optimizationLevel: 7,
+                    svgoPlugins: [
+                        { removeViewBox: true },
+                        { removeUselessStrokeAndFill: true },
+                        { removeEmptyAttrs: true }
+                    ]
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.{png,jpg,gif,svg}'],
+                    dest: 'dev/'
+                }]
+            }
+        },
+        compress: {
+            dist: {
+                options: {
+                    mode: 'gzip',
+                    pretty: true
+                },
+                expand: true,
+                cwd: 'src/',
+                src: ['**/*'],
+                dest: 'dist/'
             }
         }
-    }));
+    });
+   // Load the plugin that provides the "watch" task.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Load the plugin that provides the "htmlmin" task.
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    // Load the plugin that provides the "compress" task.
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    // Load the plugin that provides the "imagemin" task.
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    // Default Tasks
-    grunt.registerTask('default', ['uglify']);
+    // Default task runt using "grunt" on the command line
+    grunt.registerTask('default', ['htmlmin', 'imagemin', 'compress']);
 };
